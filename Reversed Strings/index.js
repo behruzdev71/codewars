d7ebc7e2651088a84d854d8eb5b83f3940a663fa
Behruz <behruz@bictory.io>
2024-01-25 14:29:50 +0500

    Reversed Strings

---
+++ Reversed Strings/index.js
@@ -0,0 +1,5 @@
+function solution(str) {
+    var splitStr = str.split('');
+    return splitStr.reverse().join('');
+}
+console.log(solution('Admin'));
---
+++ Reversed Strings/index.ts
@@ -0,0 +1,5 @@
+function solution(str: string) {
+  const splitStr = str.split('')
+
+  return splitStr.reverse().join('')
+}
